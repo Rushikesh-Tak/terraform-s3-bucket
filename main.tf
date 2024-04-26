2e72b0059ba217ee6c6ebc902f2845bca0360d4e
rkaditya <adityatak443@gmail.com>
2024-04-26 17:45:08 +0530

    changed name of bucket

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "terraform-bucket" {
-    bucket = "My-terraform-tfstate-file-bucket-1"
+    bucket = "my-terraform-tfstate-file-bucket-1"
     tags = {
-        Name = "My-terraform-tfstate-file-bucket-1"
+        Name = "my-terraform-tfstate-file-bucket-1"
         Env = "Dev"
         Owner = "Rushikesh"
     }
